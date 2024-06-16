714390451605268469590589112d86bb92170521
PaskalevStoyan <paskalev@progress.com>
2024-06-16 20:15:26 +0300

    clean navbar animation

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,22 +1,61 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./navbar.css";
+
+const linksArray = [
+  {
+    text: "About Me",
+    url: "#",
+    selected: true,
+  },
+  {
+    text: "Projects",
+    url: "#",
+    selected: false,
+  },
+  {
+    text: "Contacts",
+    url: "#",
+    selected: false,
+  },
+];
 
 export const Navbar = (props: any) => {
   const { shouldShow } = props;
 
+  const [links, setLinks] = React.useState(linksArray);
+
+  const handleOnClick = (event: any) => {
+    const target = event.target;
+
+    if (target.classList.contains("navbar-link")) {
+      const currentSelected = links.map((link) => {
+        return {
+          ...link,
+          selected: link.text === target.innerText,
+        };
+      });
+
+      setLinks(currentSelected);
+    }
+  };
   return (
     <>
       (
-      <div className={`${shouldShow ? "show" : "hide"} navbar`}>
-        <a className="navbar-link selected" href="#">
-          About Me
-        </a>
-        <a className="navbar-link" href="#">
-          Projects
-        </a>
-        <a className="navbar-link" href="#">
-          Contacts
-        </a>
+      <div
+        role="navigation"
+        onClick={handleOnClick}
+        className={`${shouldShow ? "show" : "hide"} navbar`}
+      >
+        {links.map((link, index) => (
+          <a
+            key={index}
+            className={`${link.selected ? "selected" : null} navbar-link`}
+            href="#"
+          >
+            {link.text}
+          </a>
+        ))}
       </div>
       )
     </>
